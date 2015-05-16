46792839d9678c2d2422af07590b8936bd7f13eb
Abel Ponce <abel.ponc@gmail.com>
2015-05-15 21:05:07 -0500

    Creados los archivos para WP de header y footer sin ser editables

---
+++ footer.php
@@ -0,0 +1,73 @@
+<!-- ********************* CONTACTO ********************* -->
+<article class="cont-mensaje">
+	<div class="wrapt">
+		<div class="btn-telefono">
+			<img src="<?php bloginfo(template_directory) ?>/img/telefono.svg" alt="">
+		</div>
+		<div class="mensaje btn-mensaje">
+			<header>
+				<div class="texto-cabecera-mensaje">
+					<img src="<?php bloginfo(template_directory) ?>/img/mensaje.svg" alt="">
+					<p>Texto motivador para el mensaje, puede ser cualquier cosa</p>
+				</div>
+			</header>
+			<div class="cuerpo-mensaje"></div>
+			
+		</div>
+		<article class="redes">
+			<header>siguenos en nuestras redes sociales</header>
+			<div class="cuerpo-redes">
+				<div>
+				<a href=""><section>
+						<div></div>
+						<p>facebook</p>
+					</section></a>
+				<a href=""><section>
+						<div></div>
+						<p>twitter</p>
+					</section></a>
+				<a href=""><section>
+						<div></div>
+						<p>linkedin</p>
+					</section></a>
+				<a href=""><section>
+						<div></div>
+						<p>google plus</p>
+					</section></a>
+				<a href=""><section>
+						<div></div>
+						<p>instagram</p>
+					</section></a>
+				<a href=""><section>
+						<div></div>
+						<p>youtube</p>
+					</section></a>
+				</div>
+			</div>
+		</article>
+	</div>
+
+
+</article>
+
+<!-- ********************* PIE DE PÁGINA ********************* -->
+<footer>
+	<div class="wrapt">
+		<div class="logo-blanco">
+			<img src="<?php bloginfo(template_directory) ?>/img/logo-nuvoil-blanco.svg" alt="">
+		</div>
+		<div class="dato-uno">framboyanes 3303 <br>ciudad industrial bruno pagliali <br> Veracruz, Ver. México</div>
+		<div class="dato-dos">
+			<div class="tel">+52 (229) 9893770</div>
+			<div class="correo">contacto@nuvoil.com</div>
+		</div>
+		<div class="reservados">nuvoil©2015 Todos los derechos reservados</div>
+	</div>
+</footer>
+
+<!-- ********************* SCRIPTS ********************* -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="<?php bloginfo(template_directory) ?>/js/index.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title><?php bloginfo(the_tile); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo(template_directory) ?>/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<!-- ********************* ABRE CABECERA PRINCIPAL ********************* -->	
+	<header class="cabecera-principal">
+		<section class="barra-negra">
+			<div class="wrapt">
+				<section class="idioma">
+					<div class="lenguaje">
+						<div>Es</div>
+						<div class="triangulito-lenguaje"></div>
+					</div>
+					<ul class="seleccion-lenguaje">
+						<li>español</li>
+						<li>inglés</li>
+					</ul>
+				</section>
+				<section class="bolsa-blog">
+					<ul>
+						<li><a href="">Bolsa de trabajo</a></li>
+						<li class="rojo">|</li>
+						<li><a href="">Blog</a></li>
+					</ul>
+				</section>
+				<section class="contacto">
+					<div><img src="<?php bloginfo(template_directory) ?>/img/contacto.svg" alt=""></div>
+				</section>
+			</div>
+		</section>
+
+		<section class="barra-blanca">
+			<div class="wrapt">
+				<div class="cont-logo">
+					<figure><img src="<?php bloginfo(template_directory) ?>/img/logo-nuvoil.svg" alt="" id="logo"></figure>
+				</div>
+				<div class="social"></div>
+				<div class="menu-icono">
+					<span></span>
+					<div>menu</div>
+				</div>
+				
+			</div>
+			<nav>
+					<ul>
+						<li><a href=""><p>servicios</p></a></li>
+						<li><a href=""><p>proyectos</p></a></li>
+						<li><a href=""><p>marcas</p></a></li>
+						<li><a href=""><p>sistema de gestión integral</p></a></li>
+						<li><a href=""><p>acerca de nuvoil</p></a></li>
+						<li><a href=""><p>noticias</p></a></li>
+					</ul>
+				</nav>
+		</section>
+		
+	</header>
+<div class="bloque"></div>

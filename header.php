5d14c074684d5a6bf53989703f4214316a7ea184
Abel Ponce <abel.ponc@gmail.com>
2015-05-21 02:31:42 -0500

    Creación de la página estática de Bolsa de trabajo

--- header.php
+++ header.php
@@ -4,6 +4,11 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title><?php bloginfo(the_tile); ?></title>
+	<?php if ( is_front_page() ) {?>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/stylus/inicio.css">
+	<?php } elseif ( is_page( array('bolsa-de-trabajo') ) ) { ?>
+		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/stylus/pages.css">
+	<?php } ?>
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>	
 </head>
@@ -50,12 +55,20 @@
 			</div>
 			<nav>
 					<ul>
+						<?php 
+							$args = array(
+								'theme_location' => 'primary'
+								);
+							wp_nav_menu( $args ); 
+						?>
+<!--
 						<li><a href=""><p>servicios</p></a></li>
 						<li><a href=""><p>proyectos</p></a></li>
 						<li><a href=""><p>marcas</p></a></li>
 						<li><a href=""><p>sistema de gestión integral</p></a></li>
 						<li><a href=""><p>acerca de nuvoil</p></a></li>
 						<li><a href=""><p>noticias</p></a></li>
+-->
 					</ul>
 				</nav>
 		</section>
